d8ef0da44bafc7e7fb9505cc61daaeab567b88d6
kagami <kagamin8078@protonmail.com>
2021-12-18 08:04:35 +0900

    Merge remote-tracking branch 'origin/master' 2021年12月18日 <再設計> ●「オトコチェック」アプリケーションの修正。修正箇所は下記２点。   ・モジュール「ThirdTime03」を追加し、ビジネス層に「オトコアダプター」クラスを追加。    ・モジュール「ThirdTime03」に、ビジネス層に「回答アダプター」クラスを追加。     →例えばプレゼンテーション層から受け取ったViewModelに対して、永続層に記憶させる際になにか付加情報を追加するケースなどもあるかと思い。     レイヤーアーキテクチャの基本に立ち返り、ユーザー層は直下の層であるビジネス層にしかアクセスできないように変更した。     なお、この設計指針が有益であるかはまだ不明。

--- out/production/MySystemDesignLabo.AD20211019_引数の数の異なる「オトコチェック」.ThirdTime/src/main/java/オトコチェック/user/tool/NewLine.java
+++
@@ -1,7 +0,0 @@
-package オトコチェック.user.tool;
-
-public class NewLine extends オトコチェック.business.tool.NewLine {
-    public NewLine()               { super(1); }
-    public NewLine(Integer 行数)    { super(行数); }
-    public final String executed() { return super.executed(); }
-}
