0320a324fa6033cfa3aaf5155be660ce3fd4e4ed
kagami <kagamin8078@protonmail.com>
2021-12-19 09:19:49 +0900

    Merge remote-tracking branch 'origin/master' 2021年12月19日 <再設計> ●とあるネット情報を基に「社員勤務表」アプリケーションを再設計。修正箇所は下記３点。   ・モジュール「ThirdTime05」の「ユーザー層.利用者.Pure.勤務状況サブステータス区分問合せ」メソッドにあった繰り返し処理を「ビジネス層..勤務状況サブステータス区分問合せ.問合せ回答()」メソッドに移し替え。   ・「オトコチェック」アプリケーションの「NewLine」クラスを本アプリケーションの「ビジネス層.tool」にコピペ   ・ほぼ同様の手口でユーザー層にも「NewLine」クラスを追加。   ・エラーが出たので内部を若干微修正。
    <補足>
    ●ビジネス層の「勤務状況サブステータス区分」enumクラスがだいぶ「しなやか」になってきたと「思われる」ので、そろそろビジネスロジックの根幹部である「ドメイン層」へ移し替える予定。その際に、パッケージ構成も一部変更していく予定。

--- AD20211005_「社員勤務表」の選択肢/ThirdTime05/src/main/java/社員勤務表/business/service/勤務状況サブステータス区分問合せサービス.java
+++ AD20211005_「社員勤務表」の選択肢/ThirdTime05/src/main/java/社員勤務表/business/service/勤務状況サブステータス区分問合せサービス.java
@@ -1,10 +1,14 @@
 package 社員勤務表.business.service;
 
 import 社員勤務表.business.adapter.勤務状況サブステータス区分アダプター;
+import 社員勤務表.business.tool.NewLine;
 import 社員勤務表.business.tool.区分値区切り線;
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.stream.Collectors;
 
 public class 勤務状況サブステータス区分問合せサービス {
 
@@ -28,6 +32,46 @@ public class 勤務状況サブステータス区分問合せサービス {
         else                           { my勤務状況サブステータス区分list.clear(); }
 
         return  Collections.unmodifiableList(my勤務状況サブステータス区分list);
+    }
+
+    public final String 問合せ回答(){
+
+        //初期化
+        勤務状況サブステータス区分問合せサービス my勤務状況サブステータス区分問合せ
+                = new 勤務状況サブステータス区分問合せサービス(my勤務状況);
+        my勤務状況サブステータス区分list
+                = my勤務状況サブステータス区分問合せ.勤務状況サブステータス区分List();
+
+        //加工
+        final String _result01     = "『勤務状況』が" + my勤務状況 + "の場合、該当する『勤務状況サブステータス区分』は"
+                                    + NewLine.hiSpeedExecuted();
+
+        final String _result02work = my勤務状況サブステータス区分list
+                                     .stream().map( list要素 -> " ●" + list要素.name())
+                                        .collect(Collectors.toList())
+                                        .toString();
+        //↑ cf)[ ●list要素.name(), ●list要素.name(), ●list要素.name()...]
+
+        final String _result02     = _result02work
+                                        .replace("["," ")
+                                        .replace("]","")
+                                        .replace( " ●" + new 区分値区切り線().name(),new 区分値区切り線().name())
+                                        .replace(",",NewLine.hiSpeedExecuted());
+                                        //↑ cf)
+                                        // ●list要素.name()
+                                        // ●list要素.name()
+                                        // ------------または-------------------
+                                        // ●list要素.name()
+                                        // ...
+
+        final String _result03     = NewLine.hiSpeedExecuted()
+                                    + "です。";
+
+        //連結
+        final String _result = _result01 + _result02 + _result03;
+
+        //結果を出力
+        return _result;
     }
 
     private List<勤務状況サブステータス区分インターフェース> テレワーク設定(){
--- AD20211005_「社員勤務表」の選択肢/ThirdTime05/src/main/java/社員勤務表/user/domainExpert/Pure.java
+++ AD20211005_「社員勤務表」の選択肢/ThirdTime05/src/main/java/社員勤務表/user/domainExpert/Pure.java
@@ -1,21 +1,21 @@
 package 社員勤務表.user.domainExpert;
 
 import 社員勤務表.business.service.*;
+import 社員勤務表.user.tool.NewLine;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Pure {
     private static List<勤務区分インターフェース> my勤務区分list = new ArrayList<>();
-    private static List<勤務状況サブステータス区分インターフェース> my勤務状況サブステータス区分list = new ArrayList<>();
     private static List<勤務状況サブステータス区分インターフェース> my勤務状況サブステータス区分補足説明list = new ArrayList<>();
 
-    public static void main(String args[]){
+    public static void main(String[] args){
 
         final String my勤務状況 = args[0];
 
         勤務区分問合せ(my勤務状況);
-        勤務状況サブステータス区分問合せ(my勤務状況);
+        System.out.println(勤務状況サブステータス区分問合せ(my勤務状況));
         勤務状況サブステータス区分補足説明(my勤務状況);
     }
 
@@ -28,21 +28,17 @@ public class Pure {
         for (勤務区分インターフェース my勤務区分 : my勤務区分list) {
             System.out.println("  ●" + my勤務区分.name());
         }
-        System.out.println("です。");
+        System.out.println("です。" + NewLine.hiSpeedExecuted());
     }
 
-    private static void 勤務状況サブステータス区分問合せ(String my勤務状況){
+    private static String 勤務状況サブステータス区分問合せ(String my勤務状況){
 
         勤務状況サブステータス区分問合せサービス my勤務状況サブステータス区分問合せ
                 = new 勤務状況サブステータス区分問合せサービス(my勤務状況);
-        my勤務状況サブステータス区分list
-                = my勤務状況サブステータス区分問合せ.勤務状況サブステータス区分List();
 
-        System.out.println("『勤務状況』が" + my勤務状況 + "の場合、該当する『勤務状況サブステータス区分』は");
-        for (勤務状況サブステータス区分インターフェース my勤務状況サブステータス区分 : my勤務状況サブステータス区分list) {
-            System.out.println("  ●" + my勤務状況サブステータス区分.name());
-        }
-        System.out.println("です。");
+        return my勤務状況サブステータス区分問合せ.問合せ回答()
+                + NewLine.hiSpeedExecuted()
+                ;
     }
 
     private static void 勤務状況サブステータス区分補足説明(String my勤務状況){
