9a7247f6983ee3201378617a9a0afe4326064a57
kagami <kagamin8078@protonmail.com>
2022-05-06 15:05:17 +0900

    Merge remote-tracking branch 'origin/master' 2022年05月06日 <モジュール追加> ●『ThirdTime06.社員勤務表』モジュールの 『WorkSubStatusDescriptionTranslator』クラス少し改良。外部仕様は変化なし。 ⇒これもリファクタリング!?。
    <残課題>
    『ThirdTime06.社員勤務表』モジュール全体を主に下記のように仕上げる予定。
     □『domain.anticorruption』内部のパッケージ構成を変更する(※もっと普通の構成にする)
     □『日本語または和製英語云々』なコードを、『(なるべく自然な)英語』に変更
     □元ネタが『ThirdTime04-02』なので、コード全体をも少し最新の知見で改善する
     □上記すべての対応後、最後にコード全体の見た目の改善をする(※適切な改行をするなど)

--- AD20211005_「社員勤務表」の選択肢/ThirdTime06/src/main/java/roster/domain/anticorruption/translator/WorkSubStatusDescriptionTranslator.java
+++ AD20211005_「社員勤務表」の選択肢/ThirdTime06/src/main/java/roster/domain/anticorruption/translator/WorkSubStatusDescriptionTranslator.java
@@ -7,25 +7,36 @@ import roster.domain.model.LocationDivision;
 
 public class WorkSubStatusDescriptionTranslator {
 
-    private LocationDivision myLocationDivision;
-    private 屋内区分 my屋内区分;
-    private 状態区分 my状態区分;
-    private WorkSubStatusAdapter myWorkSubStatusAdapter;
+    final private WorkSubStatusAdapter myWorkSubStatusAdapter;
 
     public WorkSubStatusDescriptionTranslator(WorkSubStatusAdapter myWorkSubStatusAdapter){
         this.myWorkSubStatusAdapter = myWorkSubStatusAdapter;
     }
 
     public final String description(){
-        return switch (myWorkSubStatusAdapter) {
-            case INSIDE  -> new InsideDescriptionTranslator().description(); //長文なので外だし
+
+        //Enumのswitch:
+        // defaultを排すると、全網羅しないとエラーになってくれるらしい。
+        final String myDescription =
+        switch (myWorkSubStatusAdapter) {
+            case INSIDE  ->  new InsideDescriptionTranslator().description(); //長文なので外だし
             case OUTSIDE -> LocationDivision.OUTSIDE.comments();
             case 稼働     -> 状態区分.稼働.補足説明();
             case 非稼働   -> 状態区分.非稼働.補足説明();
             case 内勤     -> 屋内区分.内勤.詳細説明();
             case 外勤     -> 屋内区分.外勤.詳細説明();
-            case 国外     -> "aaaa";
-            default      -> "その値は勤務状況サブステータスにはございません。";
+            case 国外     -> "aaaa" ;
          };
+
+        //念のため、例外キャッチアップ
+        if( myDescription.isEmpty() ){
+            throw new RuntimeException(
+                      "その値は勤務状況サブステータスにはございません。[区分値:"
+                    + myWorkSubStatusAdapter.name()
+                    + "]"
+            );
+        }
+
+        return myDescription;
     }
 }
